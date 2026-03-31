9ba93001ed48c8683480b4f08881f471dfc3cdc0
Arthur Kroth Posselt <akposselt@gmail.com>
2026-03-31 19:23:02 -0300

    feat: criando classe Cart.

---
+++ src/main/java/model/Cart.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Cart {
+
+    private int idCart;
+    private int idUser;
+    private int idProduct;
+    private int quantity;
+    
+    public Cart() {
+    }
+
+    public Cart(int idCart, int idUser, int idProduct, int quantity) {
+        this.idCart = idCart;
+        this.idUser = idUser;
+        this.idProduct = idProduct;
+        this.quantity = quantity;
+    }
+
+    public int getIdCart() {
+        return idCart;
+    }
+
+    public void setIdCart(int idCart) {
+        this.idCart = idCart;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart [idCart=" + idCart + ", idUser=" + idUser + ", idProduct=" + idProduct + ", quantity=" + quantity
+                + "]";
+    }
+}
