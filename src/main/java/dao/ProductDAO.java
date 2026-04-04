0368de9cd6a1a311761a4140e2bf911616bdb911
Arthur Kroth Posselt <akposselt@gmail.com>
2026-04-04 15:06:24 -0300

    feat: create ProductDAO class with all necessary imports

---
+++ src/main/java/dao/ProductDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import model.Product;
+
+public class ProductDAO {
+
+
+}
